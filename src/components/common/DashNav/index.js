8b85af55d960995226e8c84f3242f4015994c2bc
kwanj-k <mwangikelvin23@gmail.com>
2019-04-07 14:24:03 +0300

    Add logout functionality

--- src/components/common/DashNav/index.js
+++ src/components/common/DashNav/index.js
@@ -1,31 +1,57 @@
-import React from 'react';
-import ctrim from './ctrim.fgy';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'semantic-ui-react';
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 
 import './nav.css';
+import ctrim from './ctrim.fgy';
+import { logoutUser } from '../../../actions/authActions';
 
 
-const Nav = () => {
-    return (
-        <div className='nav_marg'>
-            <nav>
-                <ul>
-                    <li>
-                        <img
-                        src={ctrim}
-                        className="img-responsive"
-                        alt="+Ctrim" />
-                    </li>
-                    <li ><Link to="/newstore" >AddStore</Link></li>
-                    <li >
-                        <Button as='a' className='btn'>
+class Nav extends Component {
+
+    onClick = e => {
+        e.preventDefault();
+        this.props.logoutUser();
+        this.props.history.push('/')
+    };
+
+    render () {
+        return (
+            <div className='nav_marg'>
+                <nav>
+                    <ul>
+                        <li>
+                            <img
+                            src={ctrim}
+                            className="img-responsive"
+                            alt="+Ctrim" />
+                        </li>
+                        <li ><Link to="/newstore" >AddStore</Link></li>
+                        <li >
+                        <Button
+                            as='a'
+                            className='btn'
+                            onClick={this.onClick}>
                             Logout
                         </Button>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    )
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        )
+    }
 }
-export default Nav;
+
+const mapStateToProps = state => (
+    state
+)
+
+
+export default withRouter(
+    connect(
+      mapStateToProps,
+      { logoutUser }
+    )(Nav)
+  );
